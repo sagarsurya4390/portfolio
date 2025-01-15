bace293789848936e1a04ffa59179a074b9306d2
SagarSurya <sagar.suryawanshi20@vit.edu>
2025-01-15 17:17:30 +0530

    Setup has been completed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=''>
+      <h1 className="text-3xl text-center font-bold underline">
+      Hello world!
+    </h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
